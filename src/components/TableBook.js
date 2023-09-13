29148de7ea37ca076ac43416be380fdd6f7603ca
Rabab Ahmed Fouad <72532543+Rabab13@users.noreply.github.com>
2023-09-13 15:42:22 +0300

    Update TableBook.js

--- src/components/TableBook.js
+++ src/components/TableBook.js
@@ -1,64 +1,54 @@
 import React, { useState } from 'react';
 
+function TableBook(props) {
+  const { books, onDelete, onUpdate } = props;
 
-function TableBook (props) {
+  return (
+    <table className="table table-borderless">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {books.map((book) => {
+          return (
+            <tr key={book._id}>
+              <td className="title">{book.title}</td>
+              <td>
+                <Description book={book} onUpdate={onUpdate} />
+              </td>
+              <td>
+                <button className="delete" onClick={() => onDelete(book._id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
 
-    const {books, onDelete, onUpdate} = props;
+function Description(props) {
+  const { book, onUpdate } = props;
+  const [description, setDescription] = useState(book.description);
 
-            return (
-                <table className="table table-borderless">
-                    <tr>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Delete</th>
-                </tr>
-                    {books.map(book =>{
-                        return (
-                            <>
-                        
-                            <tr key={book._id}>
-                                <td className="title">{book.title}</td>
-                                <Description book={book} onUpdate={onUpdate}/>
-                                <td>
-                                <button className="delete" onClick={() => onDelete(book._id)}>Delete</button>
-                                </td>
-                            </tr>
-                            
-                            
-                            </>
-                        )
-                    })
-                    
-                }
-                </table>
+  const handleChange = (e) => {
+    setDescription(e.target.value);
+  };
 
-            )
-        }
-    //Create new func to update book description
-    function Description (props){
-
-        const {book, onUpdate} = props
-        const [description, setDescription] = useState(book.description);
-
-        const handleChange = (e) => {
-            setDescription(e.target.value)
-        }
-
-
-        return (
-            <tr className="description" key={book.description}>
-                <td>
-                    <input type="text" value={description} onChange ={(e)=> handleChange(e)}/>
-
-                </td>
-                <td>
-                    <button  onClick={() => onUpdate({_id: book._id, description: description})}>Update Book Description</button>
-                </td>
-                </tr>
-                
-            
-        )
-    }
-
+  return (
+    <div className="description" key={book.description}>
+      <input type="text" value={description} onChange={(e) => handleChange(e)} />
+      <button onClick={() => onUpdate({ _id: book._id, description: description })}>
+        Update Book Description
+      </button>
+    </div>
+  );
+}
 
 export default TableBook;
